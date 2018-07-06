ff8717099bdfbc37555d836c1cd07d8d6e2d0b45
xiaoyue26 <296671657@qq.com>
2018-07-06 22:20:45 +0800

    learn oj466

---
+++ src/main/java/practice/leetcode/oj461to470/OJ466.java
@@ -0,0 +1,48 @@
+package practice.leetcode.oj461to470;
+
+/**
+ * @author xiaoyue26
+ */
+public class OJ466 {
+    public int getMaxRepetitions(String s1, int n1, String s2, int n2) {
+        int[] repeatCount = new int[s2.length() + 1];
+        int[] nextIndex = new int[s2.length() + 1];
+        repeatCount[0] = 0;
+        nextIndex[0] = 0;// 第0遍扫描结束后,需要s2的第0个字符
+        int j = 0, count = 0;
+        for (int k = 1; k <= n1; k++) { // 扫n1遍
+            for (int i = 0; i < s1.length(); i++) { // 扫一遍s1
+                if (s1.charAt(i) == s2.charAt(j)) {
+                    ++j;
+                    if (j == s2.length()) {
+                        j = 0;
+                        ++count;
+                    }
+                }
+            }
+            repeatCount[k] = count;
+            nextIndex[k] = j;
+            // 检查是否发生了循环
+            for (int start = 0; start < k; start++) {
+                if (nextIndex[start] == j) {
+                    // start遍扫描后增长量
+                    int prefixCount = repeatCount[start];
+                    // (start,k]之间增长量（每个周期增长量）.
+                    // 其中: (k - start)=>周期的宽度； (n1 - start) / (k - start): 有几个周期
+                    int patternCount = (repeatCount[k] - repeatCount[start]) * (n1 - start) / (k - start);
+                    // n个周期后,剩下不足周期的部分: (n1 - start) % (k - start)
+                    int suffixCount = repeatCount[start + (n1 - start) % (k - start)] - repeatCount[start];
+                    return (prefixCount + patternCount + suffixCount) / n2;
+                }
+            }
+        }
+        // 直到最后都没循环(n1长度不够,而s2长度很长,导致没有达到抽屉原理的条件)
+        return repeatCount[n1] / n2;
+    }
+
+    public static void main(String[] args) {
+        OJ466 obj = new OJ466();
+        System.out.println(obj.getMaxRepetitions(
+                "acb", 4, "ab", 2));
+    }
+}
