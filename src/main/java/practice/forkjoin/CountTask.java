4e9b7b6153e7e8ca65334e8ee39ce76304f4c0a0
xiaoyue26 <296671657@qq.com>
2018-08-04 20:52:24 +0800

    learn forkjoin

---
+++ src/main/java/practice/forkjoin/CountTask.java
@@ -0,0 +1,51 @@
+package practice.forkjoin;
+
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ForkJoinPool;
+import java.util.concurrent.Future;
+import java.util.concurrent.RecursiveTask;
+
+/**
+ * @author xiaoyue26
+ */
+public class CountTask extends RecursiveTask<Integer> {
+    private final static int THREDSHOLD = 2;
+    private final int start;
+    private final int end;
+
+    public CountTask(int start, int end) {
+        this.start = start;
+        this.end = end;
+    }
+
+    @Override
+    protected Integer compute() {
+        int sum = 0;
+        boolean canCompute = (end - start) <= THREDSHOLD;
+        if (canCompute) {
+            for (int i = start; i <= end; i++) {
+                sum += i;
+            }
+        } else {
+            // 任务太大, 分治
+            int mid = start + (end - start) / 2;
+            CountTask left = new CountTask(start, mid);
+            CountTask right = new CountTask(mid + 1, end);
+            left.fork();
+            right.fork();
+            int leftRes = left.join();
+            int rightRes = right.join();
+            sum = leftRes + rightRes;
+        }
+        return sum;
+    }
+
+    public static void main(String[] args) throws ExecutionException, InterruptedException {
+        ForkJoinPool forkJoinPool = new ForkJoinPool();
+        CountTask task = new CountTask(1, 10);// RecursiveTask=> ForkJoinTask
+        Future<Integer> res = forkJoinPool.submit(task);// ForkJoinTask
+        System.out.print(res.get());
+
+
+    }
+}
